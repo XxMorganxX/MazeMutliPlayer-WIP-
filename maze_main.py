de0711eb0a88c23f1aa5b9860f5d580633f3f2b6
XxmorganxX <morgannstuart@gmail.com>
2022-03-09 23:06:30 -0500

    V4

--- maze_main.py
+++ maze_main.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
-import a_star, pygame, random, maze_solution
+from matplotlib.pyplot import box
+from numpy import empty
+import pygame, random, maze_solution
 
 
 """
@@ -32,17 +34,18 @@ BLACK = (0, 0, 0)
 TEAL = (0,128,128)
 ORANGE = (255, 203, 62)
 BLUE = (51, 102, 255)
+GREEN = (62, 160, 85)
 
 #Constants
-FPS = 100
+FPS = 2
 box_dimension = 15
 
 #Getting Functions
 def indexOfBox(box):
-    return (box.topleft[0] // box_dimension) + ((box.topleft[1] // box_dimension) * (dimension//box_dimension))
+    return int(box.topleft[0] // box_dimension) + ((box.topleft[1] // box_dimension) * (dimension//box_dimension))
 
 def indexAtLocation(x, y):
-    return (x // box_dimension) + ((y // box_dimension) * (dimension//box_dimension))
+    return int((x // box_dimension) + ((y // box_dimension) * (dimension//box_dimension)))
 
 def locationOfIndex(index):
     row = (index % (dimension // box_dimension)) * box_dimension
@@ -58,7 +61,8 @@ def buildBoard(dim):
         for x in range(0, dim, box_dimension):
             numArr.append(0)
     
-    index1 = random.randint(0, int(len(numArr) * .25))
+    #index1 = random.randint(0, int(len(numArr) * .25))
+    index1 = 400
     index2 = random.randint(int(len(numArr) * .75), int(len(numArr) * .99))
     
     rectArr.append(pygame.Rect(locationOfIndex(index1), (box_dimension, box_dimension)))
@@ -103,13 +107,62 @@ def changePoint(pos, numberArr, rectangleArr):
         rectangleArr.append(pygame.Rect(x, y, box_dimension, box_dimension))
         numberArr[index] = 1
     
-    print(len(rectangleArr))
+    print(indexAtLocation(rectangleArr[-1].topleft[0], rectangleArr[-1].topleft[1]))
 
     return numberArr, rectangleArr
         
-def validate(numberArr, rectangleArr): #WIP - This is supposed to validate the maze
-    maze_solution.validate_board(numberArr, rectangleArr, dimension, box_dimension)
+def validate_board(numberArr, rectanlgeArr):
+    start = indexAtLocation(rectanlgeArr[0].topleft[0], rectanlgeArr[0].topleft[1])
+    end = indexAtLocation(rectanlgeArr[1].topleft[0], rectanlgeArr[1].topleft[1])
 
+    boxPerLen = dimension // box_dimension
+    boxDrawnSize = (box_dimension, box_dimension)
+
+    solved = False
+    firstRun = True
+    closed_set = []
+    curr_set = []
+    
+    turns = 0
+
+    while turns <= 4:
+        temp = []
+        if (rectanlgeArr[1].topleft[0], rectanlgeArr[1].topleft[1]) in curr_set or (rectanlgeArr[1].topleft[0], rectanlgeArr[1].topleft[1]) in closed_set:
+            solved = True
+        if firstRun:
+            for i in [(1), (-1), (-boxPerLen), (boxPerLen)]:
+                if numberArr[start+i] == 0:
+                    currentLoc = locationOfIndex(start+i)
+                    curr_set.append((currentLoc[0], currentLoc[1]))
+                    pygame.draw.rect(WIN, BLACK, (currentLoc[0], currentLoc[1], boxDrawnSize[0], boxDrawnSize[1]))
+                    temp = curr_set
+            print("\n First Done")
+            print(curr_set)
+            
+        elif not firstRun:
+            print("Other Runs")
+            for location in curr_set:
+                index = indexAtLocation(location[0], location[1])
+                for delta in [(index+1),(index-1),(index+boxPerLen),(index-boxPerLen)]:
+                    if delta > boxPerLen and delta < (boxPerLen* dimension)-boxPerLen and delta % boxPerLen != 0 and delta % boxPerLen != 52:
+                        if ((locationOfIndex(delta)[0], locationOfIndex(delta)[1]) not in closed_set) and numberArr[delta] == 0:
+                            temp.append(locationOfIndex(delta))
+                            pygame.draw.rect(WIN, GREEN, (locationOfIndex(delta)[0], locationOfIndex(delta)[1], boxDrawnSize[0], boxDrawnSize[1]))
+            
+
+
+        for location in temp:
+            closed_set.append(location) 
+        curr_set = temp
+        
+
+        
+        firstRun = False
+        #solved = True
+        
+        turns += 1
+
+    print("\n Done \n")
     
 
 
@@ -121,8 +174,8 @@ def main():
 
     run = True
     while run:
-        WIN.fill(WHITE)
         clock.tick(FPS)
+        WIN.fill(WHITE)
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN: mouseDown = True
             elif event.type == pygame.MOUSEBUTTONUP: mouseDown = False
@@ -130,11 +183,11 @@ def main():
 
         if mouseDown:      
             pos = pygame.mouse.get_pos()
-            if pos[1] <= dimension:
+            if pos[1] < dimension:
                 num_board, rect_board = changePoint(pos, num_board, rect_board)
-            elif submit.collidepoint(pos):
+            if submit.collidepoint(pos):
                 print("Submit")
-                validate(num_board, rect_board) # W.I.P.
+                validate_board(num_board, rect_board) # W.I.P.
                 mouseDown = False
                 
 
